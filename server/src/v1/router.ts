b546cbffb7a0420bca7c5407f10f4bc64ffa7e65
Rahmowin-1st <shahbozrahmonov7707@gmail.com>
2026-08-25 15:41:18 +0500

    feat(part2): mount canonical Part 2 API router

--- server/src/v1/router.ts
+++ server/src/v1/router.ts
@@ -6,6 +6,7 @@ import { ApiError, requestContext, v1ErrorHandler } from './errors.js'
 import { registeredJobKinds } from './jobs.js'
 import { QUOTA_CONTRACTS } from './quota.js'
 import { v1ProfileRouter } from './profile.js'
+import { v1Part2Router } from './part2.js'
 import { v1StorageRouter } from './storage.js'
 import { v1StreamRouter } from './stream.js'
 
@@ -36,6 +37,7 @@ router.get('/health', async (_req, res, next) => {
 router.use('/auth', v1AuthRouter)
 router.use('/profile', v1ProfileRouter)
 router.use('/storage', v1StorageRouter)
+router.use(v1Part2Router)
 router.use(v1StreamRouter)
 
 router.use((_req, _res, next) => next(new ApiError(404, 'ROUTE_NOT_FOUND', 'The API route was not found.')))
